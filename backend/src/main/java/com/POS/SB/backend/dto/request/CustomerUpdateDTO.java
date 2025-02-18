6edfaf90acbb53566645eff4131d99fbb8fc0374
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-02-19 00:20:09 +0530

    CustomerUpdateDTO created

---
+++ backend/src/main/java/com/POS/SB/backend/dto/request/CustomerUpdateDTO.java
@@ -0,0 +1,8 @@
+package com.POS.SB.backend.dto.request;
+
+public class CustomerUpdateDTO {
+    private int CustomerID;
+    private String customerName;
+    private String customerAddress;
+    private Double salary;
+}
