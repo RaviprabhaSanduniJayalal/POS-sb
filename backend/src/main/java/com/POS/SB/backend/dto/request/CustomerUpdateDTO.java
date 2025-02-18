6a4398b95cf3947718a3edc31c3e51515641b71e
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-02-19 00:23:28 +0530

    CustomerUpdateDTO modified

--- backend/src/main/java/com/POS/SB/backend/dto/request/CustomerUpdateDTO.java
+++ backend/src/main/java/com/POS/SB/backend/dto/request/CustomerUpdateDTO.java
@@ -5,4 +5,47 @@ public class CustomerUpdateDTO {
     private String customerName;
     private String customerAddress;
     private Double salary;
+
+    public CustomerUpdateDTO() {
+    }
+
+    public CustomerUpdateDTO(int customerID, String customerName, String customerAddress, Double salary) {
+        this.CustomerID = customerID;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.salary = salary;
+    }
+
+    public int getCustomerID() {
+        return CustomerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        CustomerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }
+
