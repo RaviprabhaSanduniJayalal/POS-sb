24b12ee6d0a14f181cb8d7fda21ba0cac4382891
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-02-25 18:24:27 +0530

    getAllCustomers added

--- backend/src/main/java/com/POS/SB/backend/service/IMPL/CustomerService.java
+++ backend/src/main/java/com/POS/SB/backend/service/IMPL/CustomerService.java
@@ -3,6 +3,8 @@ package com.POS.SB.backend.service.IMPL;
 import com.POS.SB.backend.dto.CustomerDTO;
 import com.POS.SB.backend.dto.request.CustomerUpdateDTO;
 
+import java.util.List;
+
 public interface CustomerService {
     String saveCustomer(CustomerDTO customerDTO);
 
@@ -11,4 +13,6 @@ public interface CustomerService {
     String updateCustomer(CustomerUpdateDTO customerUpdateDTO);
 
     CustomerDTO getCustomerByID(int customerId);
+
+    List<CustomerDTO> getAllCustomers();
 }
