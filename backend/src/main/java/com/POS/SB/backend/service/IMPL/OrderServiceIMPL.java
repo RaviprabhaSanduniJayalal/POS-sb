4dc0cd59e342670b7e8b3d39121e0616c8aae204
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-03-23 15:46:12 +0530

    add order method created

--- backend/src/main/java/com/POS/SB/backend/service/IMPL/OrderServiceIMPL.java
+++ backend/src/main/java/com/POS/SB/backend/service/IMPL/OrderServiceIMPL.java
@@ -1,5 +1,8 @@
 package com.POS.SB.backend.service.IMPL;
 
+import com.POS.SB.backend.dto.request.RequestOrderSaveDTO;
+import com.POS.SB.backend.entity.Order;
+import com.POS.SB.backend.entity.OrderDetails;
 import com.POS.SB.backend.repo.CustomerRepo;
 import com.POS.SB.backend.repo.ItemRepo;
 import com.POS.SB.backend.repo.OrderDetailsRepo;
@@ -7,8 +10,11 @@ import com.POS.SB.backend.repo.OrderRepo;
 import com.POS.SB.backend.service.OrderService;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -28,5 +34,30 @@ public class OrderServiceIMPL implements OrderService {
     @Autowired
     private OrderDetailsRepo orderDetailsRepo;
 
+    @Transactional
+    @Override
+    public String addOrder(RequestOrderSaveDTO requestOrderSaveDTO) {
+        Order order = new Order(
+                customerRepo.getById(requestOrderSaveDTO.getCustomers()),
+                requestOrderSaveDTO.getDate(),
+                requestOrderSaveDTO.getTotal()
+        );
+        orderRepo.save(order);
+
+        if(orderRepo.existsById(order.getOrderId())){
+
+            List<OrderDetails> orderDetails=modelMapper.map(requestOrderSaveDTO.getOrderDetails(),new TypeToken<List<OrderDetails>>(){}.getType());
+
+            for(int i=0; i<orderDetails.size(); i++){
+                orderDetails.get(i).setOrders(order);
+                orderDetails.get(i).setItems(itemRepo.getById(requestOrderSaveDTO.getOrderDetails().get(i).getItems()));
+            }
+            if(orderDetails.size()>0){
+                orderDetailsRepo.saveAll(orderDetails);
+            }
+            return "saved successfully";
+        }
+        return " error";
+    }
 
 }
