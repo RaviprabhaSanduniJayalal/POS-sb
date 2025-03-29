4430302fc330d58f0179b40a771b15fb41bc6a7a
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-03-29 12:43:18 +0530

    save customer created

--- backend/src/main/java/com/POS/SB/backend/controller/CustomerController.java
+++ backend/src/main/java/com/POS/SB/backend/controller/CustomerController.java
@@ -1,10 +1,9 @@
 package com.POS.SB.backend.controller;
 
+import com.POS.SB.backend.dto.CustomerDTO;
 import com.POS.SB.backend.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/Customer")
@@ -13,6 +12,14 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @PostMapping("/save")
+    public String saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        // CustomerServiceIMPL customerServiceIMPL=new CustomerServiceIMPL();
+        //customerServiceIMPL.saveCustomer(customerDTO);
+
+        customerService.saveCustomer(customerDTO);
+        return "customer saved successfully ";
+    }
 
 }
 
