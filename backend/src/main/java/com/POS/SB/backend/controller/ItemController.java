1fe44a656dd18767ad6a052927a89efd7e6cb5a1
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-04-22 20:04:44 +0530

    created endpoints on item

--- backend/src/main/java/com/POS/SB/backend/controller/ItemController.java
+++ backend/src/main/java/com/POS/SB/backend/controller/ItemController.java
@@ -1,12 +1,15 @@
 package com.POS.SB.backend.controller;
 
 import com.POS.SB.backend.dto.request.ItemSaveRequestDTO;
+import com.POS.SB.backend.dto.response.ItemGetResponseDTO;
 import com.POS.SB.backend.service.ItemService;
 import com.POS.SB.backend.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/Item")
@@ -22,5 +25,24 @@ public class ItemController {
                 new StandardResponse(201,"Success",message),
                 HttpStatus.CREATED
         );
+    }
+    @GetMapping(
+            path="/get-by-name",
+            params="name"
+    )
+    public List<ItemGetResponseDTO> getItemNameAndStatus(@RequestParam (value="name")String itemName) {
+        List<ItemGetResponseDTO> itemDTOS = itemservice.getItemNameAndStatus(itemName);
+        return itemDTOS;
+
+    }
+
+    @GetMapping(
+            path="/get-by-name-with-mapstruct",
+            params="name"
+    )
+    public List<ItemGetResponseDTO> getItemNameAndStatusByMapStruct(@RequestParam (value="name")String itemName) {
+        List<ItemGetResponseDTO> itemDTOS = itemservice.getItemNameAndStatusBymapStruct(itemName);
+        return itemDTOS;
+
     }
 }
